6d34ec15fd266dda2cd979b86384b7efb2929bf7
mohseen1985 <ghorbani.mohsenn@gmail.com>
2022-09-18 18:55:39 +0430

    revert mistake commit

--- src/js/ApiAddress.js
+++
@@ -1,8 +0,0 @@
-class ApiAddress {
-    constructor(){
-        this.url = "https://api.nasa.gov/planetary/apod";
-        this.apiKey = "jhCI4cTeo66JGjuWwD4cDASWcn9HeUyRtFt7OnQH";
-        this.count = 12;
-    }
-    getApiURL = () => `${this.url}?api_key=${this.apiKey}&count=${this.count}`;
-}
--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,27 @@
 window.onload = () => {
   loadData();
+ setInterval(loadData,3000) ;
 }
 loadData = () => {
- const url = new ApiAddress().getApiURL();
- const handler = new ApiHandler();
- const template = document.getElementById("spaceItemsTemplate").innerHTML;
- const mainSection = document.getElementById('mainSection');
-  handler.getData(url, function (data){
-    data.forEach(element => {
-      if(element.media_type == 'image'){
-        let currentTemplate = template;
-        currentTemplate = currentTemplate.replace('__IMAGE__',element.url);
-        currentTemplate = currentTemplate.replace('__TITLE__', element.title);
-        currentTemplate = currentTemplate.replace('__HDURL__', element.hdurl);
-        mainSection.innerHTML += currentTemplate;
-      }
-    });
-  }, function (error){
-  });
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      const json = this.responseText;
+      const dataList = JSON.parse(json);
+      const table = document.getElementById("mainTable");
+       table.innerHTML = "";
+      dataList.forEach((element,index) => {
+      let template = '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">';
+      template += `<td class="py-4 px-6">${index+1}</td>`;
+      template += `<td class="py-4 px-6">${element.Operator}</td>`;
+      template += `<td class="py-4 px-6">${element.Code}</td>`;
+      template += `<td class="py-4 px-6">${element.Amount}</td>`;
+      template += `<td class="py-4 px-6">${element.CreatedDate}</td>`;
+      template += `</tr>`;
+         table.innerHTML += template;
+      });
+    }
+  };
+  xhttp.open("GET","../../src/json/charges.json");
+  xhttp.send();
 }
